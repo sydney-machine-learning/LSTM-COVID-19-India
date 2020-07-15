2bca8005a7d275a0d465d140c01d6fa6c2640731
Ayush Jain <ayushjai@iitg.ac.in>
2020-07-15 20:45:00 +0530

    Add files via upload

---
+++ Data/extract_data.py
@@ -0,0 +1,43 @@
+import requests
+import json
+import csv
+from datetime import date 
+
+# Calling the response
+url = "https://covid-19-india-data-by-zt.p.rapidapi.com/GetIndiaTotalCounts"
+headers = {
+    'x-rapidapi-host': "covid-19-india-data-by-zt.p.rapidapi.com",
+    'x-rapidapi-key': "ec9b13e044msh53a46212a68a94dp1a0fb6jsn5ee41f18f4e1"
+    }
+response = requests.request("GET", url, headers=headers)
+
+#Fetching the json data
+json_data = json.loads(response.text)
+
+# open a file for writing raw data 
+file_name = str(date.today()) + "-overall.csv"
+data_file = open(file_name, 'w') 
+  
+# create the csv writer object 
+csv_writer = csv.writer(data_file) 
+  
+# Counter variable used for writing  
+# headers to the CSV file 
+count = 0
+for raw in json_data['data']: 
+    if count == 0: 
+  
+        # Writing headers of CSV file
+        header = []
+        for head in raw:
+            header.append(head) 
+        csv_writer.writerow(header) 
+        count += 1
+  
+    # Writing data of CSV file 
+    val = []
+    for head in raw:
+        val.append(raw[head])
+    csv_writer.writerow(val) 
+
+data_file.close() 
